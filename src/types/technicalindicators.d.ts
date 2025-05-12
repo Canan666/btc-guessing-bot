dd6d05731cbe1f9c7fa2fd1eb65918e6fe252d0f
Canan <C33556266@gmail.com>
2025-05-12 22:11:55 +0800

    Create technicalindicators.d.ts

---
+++ src/types/technicalindicators.d.ts
@@ -0,0 +1,8 @@
+// src/types/technicalindicators.d.ts
+declare module 'technicalindicators' {
+  export const RSI: any;
+  export const MACD: any;
+  // 如果后续还要用到其他指标，这里继续声明：
+  // export const BollingerBands: any;
+  // export const KDJ: any;
+}
